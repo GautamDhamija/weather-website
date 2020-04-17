5e51b64ea84d529194165308fb18e28e0443bac6
GautamDhamija <45586761+GautamDhamija@users.noreply.github.com>
2020-04-17 20:20:26 +0530

    feature addition: feels like, General Description

--- public/js/app.js
+++ public/js/app.js
@@ -11,13 +11,13 @@ weatherForm.addEventListener('submit',(e)=>{
 messageOne.textContent='Loading......'
 
 fetch('/weather?address='+location)
-.then(response=>{
+.then(response=>{   
     response.json().then((data)=>{
         if (data.error)
         return messageTwo.textContent=data.error
         console.log(data)
         messageOne.textContent=data.address
-        messageTwo.textContent='The temp is : '+data.temp.temp+'. The Dew Point is '+data.temp.dew_point
+        messageTwo.textContent='The temp is : '+data.temp.temp+' but it feels like '+data.temp.feels_like+'.The Dew point is: '+data.temp.dew_point+' .General Description : '+data.temp.weatherDescription
         
     })
 })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,12 +1,15 @@
 const axios= require('axios')
 const forecast= (longitude,latitide,callback)=>{
     const key='22ed6ecd1c2f781a7ad450b7af01b5aa'
+    //url=https://api.openweathermap.org/data/2.5/onecall?lat=30.34&lon=76.379997&units=metric&appid=22ed6ecd1c2f781a7ad450b7af01b5aa
     const url='https://api.openweathermap.org/data/2.5/onecall?lat='+latitide+'&lon='+longitude+'&units=metric&appid='+key
     axios.get(url).then((response)=>{
         
             callback(undefined,{
                 temp:response.data.current.temp,
-                dew_point: response.data.current.dew_point
+                dew_point: response.data.current.dew_point,
+                feels_like: response.data.current.feels_like,
+                weatherDescription: response.data.current.weather[0].description
             })
 
       
